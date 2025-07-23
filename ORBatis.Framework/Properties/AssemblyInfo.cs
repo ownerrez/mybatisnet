26a2b13a37e5084782ee8bb9fdd58144f65fe663
NLilley <nicholaslilley@gmail.com>
2025-07-23 14:05:10 +0300

    Rename projects and folders for consistency

--- ORBatis.Framework/Properties/AssemblyInfo.cs
+++ ORBatis.Framework/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ORBatisFramework")]
+[assembly: AssemblyTitle("ORBatis.Framework")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ORBatisFramework")]
+[assembly: AssemblyProduct("ORBatis.Framework")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
