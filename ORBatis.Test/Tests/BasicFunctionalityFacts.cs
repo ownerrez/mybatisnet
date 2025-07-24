a40355f7365e8baac53f4642ea43ab29e1efced3
NLilley <nicholaslilley@gmail.com>
2025-07-24 17:01:07 +0300

    Remove duplication. Replicate the ORApplication code sharing technique.

---
+++ ORBatis.Test/Tests/BasicFunctionalityFacts.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Xml;
+using IBatisNet.DataMapper.Configuration;
+using ORBatis.Test.Common;
+using ORBatis.Test.Common.Models;
+using Xunit;
+
+namespace ORBatis.Test.Tests;
+
+public class Tests
+{
+    private static XmlDocument GetSqlMapConfig()
+    {
+        var path = $"ORBatis.Test.Config.SqlMap.config, ORBatis.Test";
+        var sqlMapConfig = IBatisNet.Common.Utilities.Resources.GetEmbeddedResourceAsXmlDocument(path);
+        return sqlMapConfig;
+    }
+    
+    [Fact]
+    public void Basic_AbleToSelectHolidays()
+    {
+        var sqlMapConfig = GetSqlMapConfig();
+        var builder = new DomSqlMapBuilder()
+        {
+            Properties = new NameValueCollection()
+            {
+                { "ConnectionString", Constants.ConnectionString }
+            }
+        };
+
+        var mapper = builder.Configure(sqlMapConfig);
+        var context = mapper.CreateSqlMapSession();
+
+        Hashtable parameters = new Hashtable
+        {
+            { "active", true },
+            { "userId", 347317427 }
+        };
+        var holidays = mapper.QueryForList<Holiday>("Holiday.SelectAll", parameters, context);
+        Assert.NotNull(holidays);
+
+        // This one should use the previously LazyLoaded version!
+        var holidays2 = mapper.QueryForList<Holiday>("Holiday.SelectAll", parameters, context);
+        Assert.NotNull(holidays2);
+
+        var cannedParams = new Hashtable()
+        {
+            { "orderBy", "Id" },
+            { "orderDirection", "Desc" },
+            { "startAtRowNumber", 0 },
+            { "pageSize", 10 }
+        };
+        var cannedQueries = mapper.QueryForList(
+            "CannedQuery.GridForOverview",
+            cannedParams,
+            context
+        );
+        Assert.NotNull(cannedQueries);
+    }
+}
